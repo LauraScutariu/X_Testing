4d7469145317dbb6f0fe2bfba910bc4779763b76
Laura Scutariu <138318963+LauraScutariu@users.noreply.github.com>
2024-01-15 10:41:37 +0100

    m

--- steps/create_account.py
+++ steps/create_account.py
@@ -8,7 +8,7 @@ class sign_in_page(Browser):
     password = (By.XPATH,'//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]')
     sign_in_button =(By.XPATH,"//*[@id='react-root']/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/div[5]/a/div)")
 
-    def log_in(self,log_email,password):
+    def test_log_in(self,log_email,password):
         button = WebDriverWait(self.chrome, 5).until(
             EC.presence_of_element_located((By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/div[5]/a/div')))
         self.chrome.find_element(*self.log_email).send_keys(log_email)
